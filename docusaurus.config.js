4a1482a1c57352c1e0d98428faa5a428e564a5ba
oseer williams <oseer.williams@gmail.com>
2023-12-05 20:24:48 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://owilliams320.github.io/td-ai-unlimited-docs',
+  url: 'https://owilliams320.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/td-ai-unlimited-docs',
