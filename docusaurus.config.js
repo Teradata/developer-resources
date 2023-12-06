bf71f6aad45175d63fb3df1ffee89e1b477ab61c
oseer williams <oseer.williams@gmail.com>
2023-12-05 20:24:34 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://owilliams320.github.io/td-ai-unlimited-docs',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/td-ai-unlimited-docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
