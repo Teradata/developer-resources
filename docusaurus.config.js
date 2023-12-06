dbe59e1aa4be1c5050e2e50a8ef4f2d3f7ccb0de
oseer <oseer.williams@teradata.com>
2023-12-05 20:32:58 -0500

    update github links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/owilliams320/td-ai-unlimited-docs/docs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/owilliams320/td-ai-unlimited-docs/blog',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -124,7 +124,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/Teradata/ai-unlimited',
               },
             ],
           },
