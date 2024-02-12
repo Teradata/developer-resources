bb5819a5783648032f5a78898b0dc037408e143c
Oseer Williams <oseer.williams@gmail.com>
2024-02-12 12:20:18 -0500

    fix(build): fixing the links

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/install-ai-unlimited">
             Get Started - 5min ⏱️
           </Link>
         </div>
