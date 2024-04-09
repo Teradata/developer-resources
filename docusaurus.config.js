c7dfad8ebb197875d044baa30526fc9eba369ec1
Oseer Williams <oseer.williams@gmail.com>
2024-04-09 17:08:56 -0400

    feat(css): adding tailwind css for testing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,6 +5,7 @@
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
 import {themes as prismThemes} from 'prism-react-renderer';
+import tailwindPlugin from './plugins/tailwind-config.cjs';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -50,6 +51,7 @@ const config = {
   },
 
   plugins: [
+    tailwindPlugin,
     [
       '@docusaurus/plugin-content-blog',
       {
