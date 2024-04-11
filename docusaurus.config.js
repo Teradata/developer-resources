4e75094a3faa8e9a847ccb5cda069dcdb358ebca
bsahitya <sahitya.buddharaju@gmail.com>
2024-04-11 16:14:47 -0500

    feat(footer): add footer component to docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -197,6 +197,211 @@ const config = {
           },
         ],
       },
+      footerItems: {
+        links: [
+          {
+            title: 'How we help',
+            items: [
+              {
+                label: 'Roles',
+                href: 'https://www.teradata.com/how-we-help',
+              },
+              {
+                label: 'Industries',
+                href: 'https://www.teradata.com/industries',
+              },
+              {
+                label: 'Partners',
+                href: 'https://www.teradata.com/partners',
+              },
+              {
+                label: 'Partner registration',
+                external: true,
+                href: 'https://www.teradata.com/getattachment/49d347ac-d544-4a6c-9db3-27c9cd2d5afd/new-user-registration-instructions-for-td-partner-community.pdf?lang=en-us',
+              },
+              {
+                label: 'Partner portal login',
+                external: true,
+                href: 'https://teradata.my.site.com/teradataPRM/s/login/',
+              },
+              {
+                label: 'Success stories',
+                href: 'https://www.teradata.com/customers',
+              },
+            ],
+          },
+          {
+            title: 'Our platform',
+            items: [
+              {
+                label: 'Platform',
+                href: 'https://www.teradata.com/platform',
+              },
+              {
+                label: 'VantageCloud',
+                href: 'https://www.teradata.com/platform/vantagecloud',
+              },
+              {
+                label: 'ClearScape Analytics',
+                href: 'https://www.teradata.com/platform/clearscape-analytics',
+              },
+              {
+                label: 'Unleash AI Innovation',
+                href: 'https://www.teradata.com/platform/ai-ml',
+              },
+              {
+                label: 'Workloads',
+                href: 'https://www.teradata.com/platform/workloads',
+              },
+              {
+                label: 'Deployment',
+                href: 'https://www.teradata.com/platform/deployment',
+              },
+            ],
+          },
+          {
+            title: 'Getting started',
+            items: [
+              {
+                label: 'Pricing',
+                href: 'https://www.teradata.com/getting-started/pricing',
+              },
+              {
+                label: 'Demos',
+                href: 'https://www.teradata.com/getting-started/demos',
+              },
+              {
+                label: 'Modernization workshop',
+                href: 'https://www.teradata.com/getting-started/modernization-workshop',
+              },
+              {
+                label: 'Executive Briefing Center',
+                href: 'https://www.teradata.com/getting-started/executive-briefing-center',
+              },
+              {
+                label: 'Events',
+                href: 'https://www.teradata.com/events',
+              },
+              {
+                label: 'Learning',
+                href: 'https://www.teradata.com/university',
+              },
+            ],
+          },
+          {
+            title: 'Insights',
+            items: [
+              {
+                label: 'AI/ML',
+                href: 'https://www.teradata.com/insights/ai-and-machine-learning',
+              },
+              {
+                label: 'Cloud data analytics',
+                href: 'https://www.teradata.com/insights/cloud-data-analytics',
+              },
+              {
+                label: 'Data architecture',
+                href: 'https://www.teradata.com/insights/data-architecture',
+              },
+              {
+                label: 'Data platform',
+                href: 'https://www.teradata.com/insights/data-platform',
+              },
+              {
+                label: 'Data security',
+                href: 'https://www.teradata.com/insights/data-security',
+              },
+              {
+                label: 'Glossary',
+                href: 'https://www.teradata.com/glossary',
+              },
+            ],
+          },
+          {
+            title: 'About us',
+            items: [
+              {
+                label: 'Who we are',
+                href: 'https://www.teradata.com/about-us',
+              },
+              {
+                label: 'Our leadership',
+                href: 'https://www.teradata.com/about-us/leadership',
+              },
+              {
+                label: 'Newsroom',
+                href: 'https://www.teradata.com/newsroom',
+              },
+              {
+                label: 'Careers',
+                external: true,
+                href: 'https://careers.teradata.com/?_gl=1*ebzjes*_ga*MjIwNzU5MDQuMTcxMDc3MDE3MQ..*_ga_7PE2TMW3FE*MTcxMjc3NjAwNy4yOS4wLjE3MTI3NzYwMDcuNjAuMC4w',
+              },
+              {
+                label: 'Investors',
+                external: true,
+                href: 'https://investor.teradata.com/?_gl=1*ebzjes*_ga*MjIwNzU5MDQuMTcxMDc3MDE3MQ..*_ga_7PE2TMW3FE*MTcxMjc3NjAwNy4yOS4wLjE3MTI3NzYwMDcuNjAuMC4w',
+              },
+            ],
+          },
+        ],
+        socialLinks: {
+          title: 'Social media',
+          items: [
+            {
+              type: 'linkedin',
+              href: 'https://www.linkedin.com/company/teradata',
+            },
+            {
+              type: 'twitter',
+              href: 'https://twitter.com/teradata',
+            },
+            {
+              type: 'facebook',
+              href: 'https://www.facebook.com/Teradata',
+            },
+            {
+              type: 'instagram',
+              href: 'https://www.instagram.com/teradata/',
+            },
+            {
+              type: 'youtube',
+              href: 'https://www.youtube.com/user/teradata',
+            },
+            {
+              type: 'rss',
+              href: 'https://www.teradata.com/rss',
+            },
+          ],
+        },
+        legalLinks: [
+          {
+            label: 'Privacy',
+            href: 'https://www.teradata.com/privacy',
+          },
+          {
+            label: 'Don’t Sell/Share My Info',
+            href: 'https://www.teradata.com/privacy#do-not-sell-share',
+          },
+          {
+            label: 'Terms of use',
+            href: 'https://www.teradata.com/legal/terms-of-use',
+          },
+          {
+            label: 'Legal',
+            href: 'https://www.teradata.com/legal/legal-notice',
+          },
+          {
+            label: 'Support Policies',
+            href: 'https://www.teradata.com/product-support-policy',
+          },
+          {
+            label: 'Tracking Consent',
+            href: 'https://www.teradata.com/how-we-help#tracking-consent',
+          },
+        ],
+        copyright: '2024 Teradata. All Rights Reserved',
+      },
       footer: {
         style: 'dark',
         links: [
--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Header } from '@bsahitya/td-doc-design';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { useThemeConfig } from '@docusaurus/theme-common';
+import { useNavbarSecondaryMenu } from '@docusaurus/theme-common/internal';
 
 export default function Navbar() {
+  const { navItems } = useThemeConfig();
   const {
-    siteConfig: {
-      themeConfig: { navItems },
-    },
-  } = useDocusaurusContext();
+    navItems: nestedNavItems,
+    title,
+    headerActions,
+    languages,
+  } = navItems;
+  const secondaryNavbar = useNavbarSecondaryMenu();
+  console.log(secondaryNavbar);
+
   return (
-    <Header
-      navItems={navItems.navItems}
-      title={navItems.title}
-      headerActions={navItems.headerActions}
-      languages={navItems.languages}
-    ></Header>
+    <>
+      <Header
+        navItems={nestedNavItems}
+        title={title}
+        headerActions={headerActions}
+        languages={languages}
+      ></Header>
+      {secondaryNavbar.content}
+    </>
   );
 }
