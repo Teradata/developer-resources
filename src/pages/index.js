f0cc652c6b36a97691954b029d504cec2fda5ec6
Sahitya Buddharaju <sahitya.buddharaju@gmail.com>
2024-05-07 13:22:42 -0500

    styles(landing-page): change styles as per design

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,30 +7,33 @@ import HeroImageUrl from '@site/static/img/hero.png';
 import Translate from '@docusaurus/Translate';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
-
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className={clsx('container', styles.container)}>
         <div className="">
           <Heading as="h1" className="hero__title">
             {siteConfig.title}
-          </Heading> 
+          </Heading>
           <p className="hero__subtitle">
             <Translate id="theme.tagline">{siteConfig.tagline}</Translate>
           </p>
           <div className={styles.buttons}>
             <Link
-              className="button button--secondary button--lg"
-              to="/docs/install-ai-unlimited">
-                <Translate id="theme.button">Get Started - 5min ⏱️</Translate>
+              className={clsx(
+                'button button--secondary button--lg',
+                styles.getStartedButton
+              )}
+              to="/docs/install-ai-unlimited"
+            >
+              <Translate id="theme.button">Get Started</Translate>
             </Link>
           </div>
         </div>
-        <div className={styles.heroImage} >
-            <img src={HeroImageUrl} />
-            <div className={styles.heroBlur} ></div> 
+        <div className={styles.heroImage}>
+          <img src={HeroImageUrl} />
+          <div className={styles.heroBlur}></div>
         </div>
       </div>
     </header>
@@ -38,13 +41,14 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
-      <main>
+      <main className={styles.features}>
         <HomepageFeatures />
       </main>
     </Layout>
