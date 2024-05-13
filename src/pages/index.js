378b0ee9d8fd5379cf383764f84105055867caa6
Sahitya Buddharaju <sahitya.buddharaju@gmail.com>
2024-05-13 17:05:49 -0500

    styles(landing-page): add padding to image in tablet screens

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero--primary', styles.heroBanner)}>
       <div className={clsx('container', styles.container)}>
         <div className="">
           <Heading as="h1" className="hero__title">
