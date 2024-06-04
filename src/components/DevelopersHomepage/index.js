626170b13b979049c9ce8ae4acc083253c896d70
Vega <obed.vega@teradata.com>
2024-06-03 19:00:12 -0700

    adding get started with TD

--- src/components/DevelopersHomepage/index.js
+++ src/components/DevelopersHomepage/index.js
@@ -8,6 +8,16 @@ import downloadsUrl from '@site/static/img/developers/Download.png';
 import communityUrl from '@site/static/img/developers/People.png';
 import bulletUrl from '@site/static/img/developers/Bullet.png';
 import externalUrl from '@site/static/img/developers/External.png';
+import teradataCommunityUrl from '@site/static/img/developers/Community.png';
+import githubUrl from '@site/static/img/developers/Github.png';
+import stackoverflowUrl from '@site/static/img/developers/Stackoverflow.png';
+import mediumUrl from '@site/static/img/developers/Medium.png';
+import DataScientist from '@site/static/img/developers/DataScientist.png'
+import businessLeader from '@site/static/img/developers/businessLeader.png'
+import DataLeader from '@site/static/img/developers/DataLeader.png'
+import Developer from '@site/static/img/developers/Developer.png'
+import dataEngineer from  '@site/static/img/developers/dataEngineer.png'
+import { Tabs, Tab } from './Tabs';
 
 //const FeatureTitle = 'Categories';
 const FeatureList = [
@@ -66,7 +76,7 @@ const FeatureList2 = [
     href: '/docs/explore-and-analyze-data/use-cases/',
   },
   {
-    img: gettingStartedUrl,
+    img: downloadsUrl,
     img2: externalUrl,
     title: `Teradata Tools and Utilities`,
     description: 'In this how-to we will go through the steps for connecting to Teradata Vantage from a Jupyter notebook.',
@@ -80,14 +90,14 @@ const FeatureList2 = [
     href: '/docs/explore-and-analyze-data/use-cases/',
   },
   {
-    img: gettingStartedUrl,
+    img: downloadsUrl,
     img2: externalUrl,
     title: `Vantage Analytics Library`,
     description: 'The Vantage Analytics Library (VAL) consists of a Java XSP (a SQL generator), 2 table operators (to support the KMeans and Decision Tree algorithms) and statistical te...',
     href: '/docs/whats-new/',
   },
   {
-    img: gettingStartedUrl,
+    img: downloadsUrl,
     img2: externalUrl,
     title: `Teradata JDBC Driver`,
     description: 'The Teradata JDBC Driver enables Java applications to connect to the Teradata Database.',
@@ -126,7 +136,10 @@ function FeatureStayUpToDate({ img, img2, title, description, href }) {
           <img src={img} alt={title} title={title}/>
           <img src={img2} alt={title} title={title}/>
         </div>
-        <Heading as="h3">{title}</Heading>
+        <div className={styles.stayCon}>
+          <Heading as="h3">{title}</Heading>
+        </div>
+        
         {description && <p>{description}</p>}
       </div>
     </Link>
@@ -147,7 +160,7 @@ export default function DevelopersHomepage() {
       </div>
     </section>
 
-    <section>
+    <section className={styles.features}>
       <div className={clsx('container', styles.container)}>
         <div className={clsx('row', styles.row)}>
           <div className={clsx('col', styles.col)}>
@@ -155,10 +168,55 @@ export default function DevelopersHomepage() {
             <p className={clsx(styles.secondSubheading)}>Get started with Teradata</p>
           </div>
         </div>
+        <div className={clsx('row', styles.row)}>
+          <div className='col'>
+            <Tabs>
+              <Tab label="Data Scientist">
+                  <div className={styles.head}>
+                    <h3>Train ML models in Vantage</h3>
+                  </div>
+                  <div className={styles.tabContent}>
+                    <p>There are situations when you want to quickly validate a machine learning model idea. You have a model type in mind. You don’t want to operationalize with an ML pipeline just yet. You just want to test out if the relationship you had in mind exists. Also, sometimes even your production deployment doesn’t require constant relearning with MLops. In such cases, you can use Vantage Analytics Library (VAL) and multiple ML model types it supports.</p>
+                  </div>
+                  <div>
+                    <img src={DataScientist} className={styles.image} width='498px'></img>
+                  </div>
+              </Tab>
+              <Tab label="Data Engineer">
+                  <h3>dbt with Teradata Vantage</h3>
+                  <div className={styles.tabContent}>
+                    <p>This tutorial demonstrates how to use dbt (Data Build Tool) with Teradata Vantage. It’s based on the original dbt Jaffle Shop tutorial. A couple of models have been adjusted to the SQL dialect supported by Vantage.</p>
+                    <img src={DataLeader} width='498px'></img>
+                  </div>
+              </Tab>
+              <Tab label="Developer">
+                  <h3>Connect to Vantage using JDBC</h3>
+                  <div className={styles.tabContent}>
+                    <p>This how-to demonstrates how to connect to Teradata Vantage using JDBC using a sample Java application: https://github.com/Teradata/jdbc-sample-app.</p>
+                    <img src={Developer} width='498px'></img>
+                  </div>
+              </Tab>
+              <Tab label="Database Administrator">
+                  <h3>Database Run large bulkloads efficiently with Teradata Parallel Transporter (TPT)</h3>
+                  <div className={styles.tabContent}>
+                    <p>We often have a need to move large volumes of data into Vantage. Teradata offers Teradata Parallel Transporter (TPT) utility that can efficiently load large amounts of data into Teradata Vantage. This how-to demonstrates how to use TPT. In this scenario,</p>
+                    <img src={businessLeader} width='498px'></img>
+                  </div>
+              </Tab>
+              <Tab label="IT Operations">
+                  <h3>Query data stored in object storage</h3>
+                  <div className={styles.tabContent}>
+                    <p>Native Object Storage (NOS) is a Vantage feature that allows you to query data stored in files in object storage such as AWS S3, Google GCS, Azure Blob or on-prem implementations. It’s useful in scenarios where you want to explore data without building a</p>
+                    <img src={dataEngineer} width='498px'></img>
+                  </div>
+              </Tab>
+            </Tabs>
+          </div>
+        </div>
       </div>
     </section>
 
-    <section>
+    <section className={styles.features}>
       <div className={clsx('container', styles.container)}>
         <div className={clsx('row', styles.row)}>
           <div className={clsx('col', styles.col)}>
@@ -170,6 +228,26 @@ export default function DevelopersHomepage() {
           {FeatureList2.map((props, idx) => (
             <FeatureStayUpToDate key={idx} {...props} />
           ))}
+        </div>
+      </div>
+    </section>
+
+    <section className={styles.features}>
+      <div className={clsx('container', styles.container)}>
+        <div className={clsx('row', styles.row)}>
+          <div className={clsx('col', styles.col)}>
+            <p className={clsx(styles.secondSubheading)}>Talk to Developers where you are</p>
+          </div>
+        </div>
+        <div className={clsx('row', styles.row)}>
+          <div className='col'>
+            <div className={styles.imgFoot}>
+              <img src={teradataCommunityUrl} alt="Teradata Community" title="Teradata Community"/>
+              <img src={mediumUrl} alt="Teradata Community" title="Teradata Community"/>
+              <img src={githubUrl} alt="Teradata Community" title="Teradata Community"/>
+              <img src={stackoverflowUrl} alt="Teradata Community" title="Teradata Community"/>
+            </div>
+          </div>
         </div>
       </div>
     </section>
