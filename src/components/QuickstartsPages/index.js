2d508eeb03508134d70128dc6e580d423eed7bae
Vega <obed.vega@teradata.com>
2024-06-18 18:47:52 -0700

    quickstarts

--- src/components/QuickstartsPages/index.js
+++ src/components/QuickstartsPages/index.js
@@ -2,8 +2,26 @@ import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 import Link from '@docusaurus/Link';
+import { Typography } from '@teradata-web/react-components';
 
 const FeatureTitle = 'Categories';
+const Guides = [
+  {
+    title: `Getting started with Teradata AI Unlimited`,
+    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    href: '/quickstarts/introduction/teradata-vantage-engine-architecture-and-concepts/',
+  },
+  {
+    title: 'Deploy Teradata AI Unlimited locally using Docker',
+    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    href: '/quickstarts/get-access-to-vantage/on-your-local/getting-started-vmware/',
+  },
+  {
+    title: 'Deploy Teradata AI Unlimited on AWS',
+    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    href: '/quickstarts/connect-to-vantage/install-teradata-studio-on-mac-m1-m2/',
+  }
+];
 const FeatureList = [
   {
     title: `Introduction`,
@@ -53,23 +71,38 @@ function Feature({ title, description, href }) {
     <Link to={href} className={clsx('col col--4', styles.col)}>
       <div className={clsx('doc-card', styles.card)}>
         <Heading as="h3">{title}</Heading>
-        {description && <p>{description}</p>}
+        {<p className={clsx(styles.description)}>{description}</p>}
       </div>
     </Link>
   );
 }
 
-export default function HomepageFeatures() {
+export default function Categories() {
   return (
-    <section className={styles.features}>
-      <div className={clsx('container', styles.container)}>
-        <h2>{FeatureTitle}</h2>
-        <div className={clsx('row', styles.row)}>
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+    <>
+      <section className={clsx(styles.features, styles.fGuides)}>
+        <div className={clsx('container', styles.container)}>
+            <Typography scale="headline2">Featured guides</Typography>
+            <br/>
+            <div className={clsx('row', styles.row)}>
+            {Guides.map((props, idx) => (
+              <Feature key={idx} {...props} />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className={styles.features}>
+        <div className={clsx('container', styles.container)}>
+          <Typography scale="headline2">{FeatureTitle}</Typography>
+          <br/>
+          
+          <div className={clsx('row', styles.row)}>
+            {FeatureList.map((props, idx) => (
+              <Feature key={idx} {...props} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
