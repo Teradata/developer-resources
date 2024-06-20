94d543d2e7894bf78d5862fae375bc5c6fecbefc
Vega <obed.vega@teradata.com>
2024-06-20 09:25:04 -0700

    fix broken link

--- src/components/QuickstartsPages/index.js
+++ src/components/QuickstartsPages/index.js
@@ -11,7 +11,7 @@ const Guides = [
     img: aiml,
     title: `Getting started with Teradata AI Unlimited`,
     description: 'See what AI Unlimited includes, prerequisites you need, and installation how-tos.',
-    href: '/ai-unlimited-docs/docs/install-ai-unlimited/',
+    href: '/docs/install-ai-unlimited/',
   },
   {
     img: aiml,
