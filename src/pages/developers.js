80e759c381976c94bbb9684c9f5aea3310a53dda
Vega <obed.vega@teradata.com>
2024-06-24 15:55:14 -0700

    change links

--- src/pages/developers.js
+++ src/pages/developers.js
@@ -1,17 +1,17 @@
 import clsx from 'clsx';
-//import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import DevelopersHomepage from '@site/src/components/DevelopersHomepage';
 import HeroImageUrl from '@site/static/img/developers/hero.png';
-//import Translate from '@docusaurus/Translate';
-//import Heading from '@theme/Heading';
 import styles from './index.module.css';
 import React from 'react';
 import { Banner, Button, Typography } from '@teradata-web/react-components';
 
 
 function HomepageHeader() {
+  const handleButtonClick = () => {
+    window.location.href = `quickstarts/`;
+  };
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero--primary', styles.heroBanner)}>
@@ -21,7 +21,7 @@ function HomepageHeader() {
               <div>
                 <Typography scale="body1">A scalable, on-demand compute engine in the cloud.</Typography> 
                 <br/>
-                <Button label='Learn more' primary={true} icon='fa fa-arrow-right' trailingIcon={true}></Button>
+                <Button label='Learn more' primary={true} icon='fa fa-arrow-right' trailingIcon={true} onClick={handleButtonClick}></Button>
               </div>}
             imageSrc={HeroImageUrl}
             isHero={true}
