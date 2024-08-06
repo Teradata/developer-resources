7f7a75d51bd9b5934e567cab9f495854475691fc
Sahitya Buddharaju <sahitya.buddharaju@gmail.com>
2024-08-06 16:20:25 -0500

    fix(translations): refactor translations in ai unlimited homepage

--- src/pages/ai-unlimited.js
+++ src/pages/ai-unlimited.js
@@ -1,6 +1,5 @@
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import HeroImageUrl from '@site/static/img/hero.webp';
@@ -8,15 +7,16 @@ import Translate, { translate } from '@docusaurus/Translate';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero--primary', styles.heroBanner)}>
       <div className={clsx('container', styles.container)}>
         <div>
           <Heading as="h1" className="hero__title">
-            {siteConfig.title}
+            <Translate id="home_page.title" />
           </Heading>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <p className="hero__subtitle">
+            <Translate id="home_page.tagline" />
+          </p>
           <div className={styles.buttons}>
             <Link
               className={clsx(
@@ -43,9 +43,8 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout description={siteConfig.tagline}>
+    <Layout description={translate({ message: 'home_page.tagline' })}>
       <HomepageHeader />
       <main className={styles.features}>
         <HomepageFeatures />
