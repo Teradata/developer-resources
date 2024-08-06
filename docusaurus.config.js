8c2ed2f47305641a4db51cb1f8084396b726bae0
Oseer Williams <oseer.williams@gmail.com>
2024-08-06 14:52:26 -0400

    build(): update published url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://teradata.github.io',
+  url: 'https://developers.teradata.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl,
