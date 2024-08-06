70b88523429f3f849a66120a00caa0fd59e59129
Oseer Williams <oseer.williams@gmail.com>
2024-08-06 18:43:43 -0400

    feat(apis): Adding redocly APIs and editor faq

---
+++ src/pages/apis.js
@@ -0,0 +1,61 @@
+import Link from "@docusaurus/Link";
+import { Card, Typography } from "@teradata-web/react-components";
+import Layout from "@theme/Layout";
+import styles from "./index.module.css";
+import clsx from "clsx";
+import Translate, { translate } from "@docusaurus/Translate";
+
+const apiContent = [
+  {
+    title: translate({ message: "apis.query_service" }),
+    body: translate({ message: "apis.query_service_description" }),
+  },
+  {
+    title: translate({ message: "apis.data_labs" }),
+    body: translate({ message: "apis.data_labs_description" }),
+  },
+];
+
+function CardContent({ title, body }) {
+  return (
+    <>
+      <Typography scale="headline3">{title}</Typography>
+      <p>{body}</p>
+    </>
+  );
+}
+
+export default function Apis() {
+  return (
+    <Layout>
+      <main className={styles.features}>
+        <section className={clsx("container", styles.container)}>
+          <div>
+            <Typography scale="headline2">
+              <Translate id="apis.title" />
+            </Typography>
+            <br />
+            <Typography scale="body1">
+              <Translate id="apis.description" />
+            </Typography>
+            <div className={clsx("row", styles.row, styles.apiContainer)}>
+              {apiContent.map((content, index) => (
+                <Link
+                  key={index}
+                  to={content.href}
+                  className={clsx("col col--4", styles.col)}
+                >
+                  <Card
+                    content={
+                      <CardContent title={content.title} body={content.body} />
+                    }
+                  ></Card>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
