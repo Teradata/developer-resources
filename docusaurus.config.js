bb86b3ebc3ceac1b23af69e7739d1f02e57b5fad
Sahitya Buddharaju <sahitya.buddharaju@gmail.com>
2024-08-08 11:50:15 -0500

    fix(translations): add navbar link translations

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,8 +42,8 @@ const getLocalizedTranslation = (key) => {
 };
 
 const allowedUrls = [
-  'https://www.teradata.com', 
-  'https://*.teradata.com', 
+  'https://www.teradata.com',
+  'https://*.teradata.com',
   'https://avatars.githubusercontent.com',
   'https://github.com',
   'https://kit.fontawesome.com/',
@@ -81,8 +81,9 @@ const config = {
       tagName: 'meta',
       attributes: {
         'http-equiv': 'Content-Security-Policy',
-        content:
-          `default-src 'self' 'unsafe-inline' 'unsafe-eval' blob: data: ${allowedUrls.join(' ')};`,
+        content: `default-src 'self' 'unsafe-inline' 'unsafe-eval' blob: data: ${allowedUrls.join(
+          ' '
+        )};`,
       },
     },
     {
@@ -95,11 +96,11 @@ const config = {
       },
     },
     {
-      // Add Celebrus script 
-       tagName: 'script',
-       attributes: {
-         defer: 'true',
-         src: 'https://www.teradata.com/js/Celebrus/bsci.js',
+      // Add Celebrus script
+      tagName: 'script',
+      attributes: {
+        defer: 'true',
+        src: 'https://www.teradata.com/js/Celebrus/bsci.js',
       },
     },
   ],
@@ -181,8 +182,8 @@ const config = {
         },
       }),
     ],
-     // Redocusaurus config
-     [
+    // Redocusaurus config
+    [
       'redocusaurus',
       {
         specs: [
@@ -215,246 +216,6 @@ const config = {
         apiKey: '72dfc486e67f40cf44879ef55fcdd88d',
         indexName: 'teradataio',
       },
-      footerItems: {
-        links: [
-          {
-            title: 'footer.why_teradata_title',
-            items: [
-              {
-                label: 'footer.why_teradata',
-                href: 'https://www.teradata.com/why-teradata',
-              },
-              {
-                label: 'footer.trusted_ai',
-                href: 'https://www.teradata.com/trusted-ai',
-              },
-              {
-                label: 'footer.industries',
-                href: 'https://www.teradata.com/industries',
-              },
-              {
-                label: 'footer.partners',
-                href: 'https://www.teradata.com/partners',
-              },
-              {
-                label: 'footer.partner_registration',
-                external: true,
-                href: 'https://www.teradata.com/getattachment/49d347ac-d544-4a6c-9db3-27c9cd2d5afd/new-user-registration-instructions-for-td-partner-community.pdf?lang=en-us',
-              },
-              {
-                label: 'footer.partner_portal_login',
-                external: true,
-                href: 'https://teradata.my.site.com/teradataPRM/s/login/',
-              },
-              {
-                label: 'footer.solutions',
-                href: 'https://www.teradata.com/solutions/business-use-cases',
-              },
-              {
-                label: 'footer.success_stories',
-                href: 'https://www.teradata.com/customers',
-              },
-              {
-                label: 'footer.services',
-                href: 'https://www.teradata.com/how-we-help/teradata-services',
-              },
-            ],
-          },
-          {
-            title: 'footer.our_platform',
-            items: [
-              {
-                label: 'footer.platform',
-                href: 'https://www.teradata.com/platform',
-              },
-              {
-                label: 'footer.vantagecloud',
-                href: 'https://www.teradata.com/platform/vantagecloud',
-              },
-              {
-                label: 'footer.clearscape_analytics',
-                href: 'https://www.teradata.com/platform/clearscape-analytics',
-              },
-              {
-                label: 'footer.unleash_ai_innovation',
-                href: 'https://www.teradata.com/platform/ai-ml',
-              },
-              {
-                label: 'footer.workloads',
-                href: 'https://www.teradata.com/platform/workloads',
-              },
-              {
-                label: 'footer.deployment',
-                href: 'https://www.teradata.com/platform/deployment',
-              },
-            ],
-          },
-          {
-            title: 'footer.getting_started',
-            items: [
-              {
-                label: 'footer.pricing',
-                href: 'https://www.teradata.com/getting-started/pricing',
-              },
-              {
-                label: 'footer.demos',
-                href: 'https://www.teradata.com/getting-started/demos',
-              },
-              {
-                label: 'footer.modernization_workshop',
-                href: 'https://www.teradata.com/getting-started/modernization-workshop',
-              },
-              {
-                label: 'footer.executive_briefing_center',
-                href: 'https://www.teradata.com/getting-started/executive-briefing-center',
-              },
-              {
-                label: 'footer.events',
-                href: 'https://www.teradata.com/events',
-              },
-              {
-                label: 'footer.learning',
-                href: 'https://www.teradata.com/university',
-              },
-            ],
-          },
-          {
-            title: 'footer.insights',
-            items: [
-              {
-                label: 'footer.ai_ml',
-                href: 'https://www.teradata.com/insights/ai-and-machine-learning',
-              },
-              {
-                label: 'footer.cloud_data_analytics',
-                href: 'https://www.teradata.com/insights/cloud-data-analytics',
-              },
-              {
-                label: 'footer.data_architecture',
-                href: 'https://www.teradata.com/insights/data-architecture',
-              },
-              {
-                label: 'footer.data_platform',
-                href: 'https://www.teradata.com/insights/data-platform',
-              },
-              {
-                label: 'footer.data_security',
-                href: 'https://www.teradata.com/insights/data-security',
-              },
-              {
-                label: 'footer.glossary',
-                href: 'https://www.teradata.com/glossary',
-              },
-            ],
-          },
-          {
-            title: 'footer.about_us',
-            items: [
-              {
-                label: 'footer.who_we_are',
-                href: 'https://www.teradata.com/about-us',
-              },
-              {
-                label: 'footer.our_leadership',
-                href: 'https://www.teradata.com/about-us/leadership',
-              },
-              {
-                label: 'footer.newsroom',
-                href: 'https://www.teradata.com/newsroom',
-              },
-              {
-                label: 'footer.careers',
-                external: true,
-                href: 'https://careers.teradata.com/?_gl=1*ebzjes*_ga*MjIwNzU5MDQuMTcxMDc3MDE3MQ..*_ga_7PE2TMW3FE*MTcxMjc3NjAwNy4yOS4wLjE3MTI3NzYwMDcuNjAuMC4w',
-              },
-              {
-                label: 'footer.investors',
-                external: true,
-                href: 'https://investor.teradata.com/?_gl=1*ebzjes*_ga*MjIwNzU5MDQuMTcxMDc3MDE3MQ..*_ga_7PE2TMW3FE*MTcxMjc3NjAwNy4yOS4wLjE3MTI3NzYwMDcuNjAuMC4w',
-              },
-            ],
-          },
-        ],
-        socialLinks: {
-          title: 'footer.social_media',
-          items: [
-            {
-              icon: 'fa-brands fa-linkedin-in',
-              href: 'https://www.linkedin.com/company/teradata',
-              label: 'footer.social_media.linkedin',
-            },
-            {
-              icon: 'fa-brands fa-x-twitter',
-              href: 'https://twitter.com/teradata',
-              label: 'footer.social_media.twitter',
-            },
-            {
-              icon: 'fa-brands fa-facebook-f',
-              href: 'https://www.facebook.com/Teradata',
-              label: 'footer.social_media.facebook',
-            },
-            {
-              icon: 'fa-brands fa-instagram',
-              href: 'https://www.instagram.com/teradata/',
-              label: 'footer.social_media.instagram',
-            },
-            {
-              icon: 'fa-brands fa-youtube',
-              href: 'https://www.youtube.com/user/teradata',
-              label: 'footer.social_media.youtube',
-            },
-            {
-              icon: 'fa fa-rss',
-              href: 'https://www.teradata.com/rss',
-              label: 'footer.social_media.rss',
-            },
-          ],
-        },
-        legalLinks: [
-          {
-            label: 'footer.privacy',
-            href: 'https://www.teradata.com/privacy',
-          },
-          {
-            label: 'footer.do_not_sell_share',
-            href: 'https://www.teradata.com/privacy#do-not-sell-share',
-          },
-          {
-            label: 'footer.terms_of_use',
-            href: 'https://www.teradata.com/legal/terms-of-use',
-          },
-          {
-            label: 'footer.legal',
-            href: 'https://www.teradata.com/legal/legal-notice',
-          },
-          {
-            label: 'footer.support_policy',
-            href: 'https://www.teradata.com/product-support-policy',
-          },
-          {
-            label: 'footer.tracking_consent',
-            href: 'https://www.teradata.com/how-we-help#tracking-consent',
-          },
-        ],
-        linksOfInterest: {
-          title: 'footer.also_of_interest',
-          items: [
-            {
-              label: 'footer.platform_ai_ml',
-              href: 'https://www.teradata.com/platform/ai-ml',
-            },
-            {
-              label: 'footer.big_data',
-              href: 'https://www.teradata.com/solutions/big-data',
-            },
-            {
-              label: 'footer.data_security_insights',
-              href: 'https://www.teradata.com/insights/data-security',
-            },
-          ],
-        },
-        copyright: 'footer.copyright',
-      },
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
@@ -464,8 +225,11 @@ const config = {
 };
 
 // Add header items to the themeConfig with the correct baseUrl
-if(config.themeConfig) {
-  config.themeConfig.navItems = headerItems(config.baseUrl);
+if (config.themeConfig) {
+  const locale = getCurrentLocale() === 'en' ? '' : '/' + getCurrentLocale();
+  config.themeConfig.navItems = headerItems(
+    config.baseUrl ? config.baseUrl + locale : ''
+  );
 }
 
 export default config;
