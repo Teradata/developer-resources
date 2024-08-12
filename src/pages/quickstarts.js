a9b8a611adddf25163342c8ca02f15128c460103
Vega <obed.vega@teradata.com>
2024-08-12 10:58:45 -0700

    quickstarts-english-default-translations

--- src/pages/quickstarts.js
+++ src/pages/quickstarts.js
@@ -4,8 +4,7 @@ import Layout from '@theme/Layout';
 import Head from '@docusaurus/Head';
 import QuickstartsPages from '@site/src/components/QuickstartsPages';
 import HeroImageUrl from '@site/static/img/quickstarts-hero.png';
-import Translate from '@docusaurus/Translate';
-//import Heading from '@theme/Heading';
+import { translate } from '@docusaurus/Translate';
 import styles from './index.module.css';
 import { Typography } from '@teradata-web/react-components';
 
@@ -14,21 +13,19 @@ function HomepageHeader() {
     <header className={clsx('hero--primary', styles.heroBanner, styles.ali)}>
       <div className={clsx('container', styles.container)}>
         <div >
-          <Typography scale="headline1">Getting started</Typography>
+          <Typography scale="headline1">{translate({ message: 'developers.getting_started_title' })}</Typography>
           <br/>
           <p className="hero__subtitle">
-            <Translate id="theme.tagline">
-                Discover in-depth tutorials and guides about how to use Teradata Vantage from all members of the Teradata community, including employees, partners, customers.
-            </Translate>
+            {translate({ message: 'quickstarts.tagline' })}
           </p>
           <p className="hero__subtitle">
-            Existing customer or partner? Explore courses at <Link href='https://teradata.com/University/Overview' target='_blank'>Teradata University</Link>.
+          {translate({ message: 'quickstarts.tagline_2' })} <Link href='https://teradata.com/University/Overview' target='_blank'>Teradata University</Link>.
           </p>
         </div>
         <div className={styles.heroImage}>
           <img 
             src={HeroImageUrl} 
-            alt="Two individuals collaborate at a desk with a computer, against an abstract orange and blue backdrop." 
+            alt= {translate({ message: 'quickstarts.alt-img-banner' })} 
             className={styles.imgW}
           />
           <div className={styles.heroBlur}></div>
