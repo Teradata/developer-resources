2eb60a8c7d77a1c76b92fd6b56ea317cfafcadf5
Vega <obed.vega@teradata.com>
2024-09-09 11:01:58 -0700

    fix build

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -4,17 +4,24 @@ import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function FeedbackComponent() {
 
+    const handleFeedback = (feedback) => {
+        console.log("Feedback recibido:", feedback);
+    };
+
     return (
       <BrowserOnly>
       {() => {
         const Feedback = require('@teradata-web/react-components')?.Feedback;
-        <div  className={clsx(styles.feedbackContainer)}>
-          <Feedback 
+
+        return (
+          <div className={clsx(styles.feedbackContainer)}>
+            <Feedback 
               label="Did this page help?"
               labelForFeedback="Thank you for your feedback!"
-              onFeedBack={us}
-          />       
-        </div>
+              onFeedBack={handleFeedback}  
+            />       
+          </div>
+        );
       }}
     </BrowserOnly>
     );
