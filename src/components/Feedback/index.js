e4b67a0e28db5d797b63881e4f8eedcbaeb9e54e
Vega <obed.vega@teradata.com>
2024-09-09 09:17:12 -0700

    no gtag function

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -1,12 +1,14 @@
 import clsx from 'clsx';
 import styles from './styles.module.css';
-import { useLocation } from 'react-router-dom';
+//import { useLocation } from 'react-router-dom';
 import BrowserOnly from '@docusaurus/BrowserOnly';
+import { Feedback } from '@teradata-web/react-components'; 
 
 export default function FeedbackComponent() {
-    const location = useLocation();
+  //  const location = useLocation();
 
-    function us(feedbackType) {
+//    function us(feedbackType) {
+  /*    console.log(feedbackType);
         const articleUrl = location.pathname; 
         if (typeof window !== 'undefined' && window.gtag) {
           window.gtag('event', 'feedback_click', {
@@ -14,21 +16,20 @@ export default function FeedbackComponent() {
             event_label: `Feedback Type: ${feedbackType}`, 
             event_value: articleUrl, 
           });
-        }
-      }
+        }*/
+  //  }
 
     return (
       <BrowserOnly>
-        {() => {
-          const Feedback = require('@teradata-web/react-components')?.Feedback;
-          <div  className={clsx(styles.feedbackContainer)}>
+        {() => (
+          <div className={clsx(styles.feedbackContainer)}>
             <Feedback 
                 label="Did this page help?"
                 labelForFeedback="Thank you for your feedback!"
                 onFeedBack={us}
             />       
           </div>
-        }}
+        )}
       </BrowserOnly>
     );
 }
