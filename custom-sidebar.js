6e82764e86c8db8f86cae46392db2cb0d63b3d37
“bsahitya” <sahitya.buddharaju@gmail.com>
2024-11-05 17:40:08 -0600

    feat(sidebar): custom sidebar config for ai unlimited docs

---
+++ custom-sidebar.js
@@ -0,0 +1,29 @@
+// custom-sidebar.js
+
+// Custom function to modify sidebar items
+function enhanceSidebarItems(items, dirName) {
+  // Ignore files under the fabric folder in the ai-unlimited sidebar
+  if (dirName !== 'fabric') {
+    items = items.filter(
+      (item) =>
+        (item.type === 'doc' && item.id.split('/')[0] !== 'fabric') ||
+        (item.type === 'category' && item.link.id.split('/')[0] !== 'fabric')
+    );
+  }
+  return items;
+}
+
+// Custom sidebarItemsGenerator function
+async function customSidebarItemsGenerator({
+  defaultSidebarItemsGenerator,
+  ...args
+}) {
+  const { dirName } = args.item;
+  const sidebarItems = await defaultSidebarItemsGenerator(args);
+
+  return enhanceSidebarItems(sidebarItems, dirName);
+}
+
+module.exports = {
+  sidebarItemsGenerator: customSidebarItemsGenerator,
+};
