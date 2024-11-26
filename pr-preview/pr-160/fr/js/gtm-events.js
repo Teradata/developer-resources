d70814831b7729446bf32ba71ac10695b90b7637
ObedVega <ObedVega@users.noreply.github.com>
2024-11-26 20:46:38 +0000

    Deploy preview for PR 160 🛫

--- pr-preview/pr-160/fr/js/gtm-events.js
+++ pr-preview/pr-160/fr/js/gtm-events.js
@@ -1,20 +1,18 @@
-// this script instruments elements to emit GTM events
-for (let e of document.getElementsByClassName("emits-gtm-events")) {
-    e.addEventListener("click", function () {
-      dataLayer.push({
-        event: e.id,
-      });
-    });
+
+  document.addEventListener("click", function (event) {
+    const target = event.target;
   
-    const hljsElem = e.getElementsByClassName("hljs")[0];
-    if (hljsElem) {
-      hljsElem.addEventListener("copy", function () {
-        console.log("copy event");
-        dataLayer.push({
-          event: "copy_code",
-          element: e.id,
-        });
+    if (target.closest(".emits-gtm-events")) {
+      const id = target.closest(".emits-gtm-events").id;
+      window.dataLayer.push({
+        event: id,
       });
     }
-  }
+  
+    if (target.closest("cv-code-snippet")) {
+      window.dataLayer.push({
+        event: "copy_code",
+      });
+    }
+  });
   
