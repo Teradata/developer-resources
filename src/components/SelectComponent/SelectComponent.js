1f15cfd95646a151aeef5df8b0b034ddff59f185
Vega <obed.vega@teradata.com>
2024-12-23 13:07:23 -0800

    feat(quickstars-page): removing the editon option

--- src/components/SelectComponent/SelectComponent.js
+++ src/components/SelectComponent/SelectComponent.js
@@ -1,33 +1,46 @@
 import React, { useState } from 'react';
 import styles from './styles.module.css';
+import { useLocation } from 'react-router-dom';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 export default function SelectComponent() {
   const [selectedOption, setSelectedOption] = useState('');
-  const [isFocused, setIsFocused] = useState(false); // Controlar el enfoque
+  const [isFocused, setIsFocused] = useState(false);
+  const location = useLocation();
+
+  const normalizePath = (path) => path.replace(/\/$/, ''); 
+
+  const pathsNoAlert = normalizePath(`/quickstarts/`);
+
+  const currentPath = normalizePath(location.pathname);
+
+  const shouldDisplayAlert = !currentPath.includes(pathsNoAlert);  
+
 
   const handleChange = (event) => {
     setSelectedOption(event.target.value);
+
     if (event.target.value === 'cloud-service-provider') {
-        window.location.href = '/ai-unlimited/install-ai-unlimited/';
-      } else if (event.target.value === 'microsoft-fabric') {
-        window.location.href = '/ai-unlimited/fabric/get-started/';
-      }
+      window.location.href = '/ai-unlimited/install-ai-unlimited/';
+    } else if (event.target.value === 'microsoft-fabric') {
+      window.location.href = '/ai-unlimited/fabric/get-started/';
+    }
   };
 
-  const handleFocus = () => {
-    setIsFocused(true); // Mostrar el label al enfocar
-  };
-
-  const handleBlur = () => {
-    setIsFocused(false); // Ocultar el label si no hay selección
-  };
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(false);
 
   const shouldShowLabel = isFocused || selectedOption !== '';
 
+  if (!shouldDisplayAlert) return null;
+
   return (
     <div className={styles.customContainer}>
-      {/* Mostrar label solo si hay selección o el select está enfocado */}
-      {shouldShowLabel && <label className={styles.label}>Platform</label>}
+      {shouldShowLabel && (
+        <label className={styles.label} htmlFor="select-screen">
+          Platform
+        </label>
+      )}
 
       <select
         id="select-screen"
@@ -37,7 +50,7 @@ export default function SelectComponent() {
         onFocus={handleFocus}
         onBlur={handleBlur}
       >
-        <option value="" disabled>
+        <option value="" disabled hidden>
           Platform
         </option>
         <option value="cloud-service-provider">Cloud service provider</option>
