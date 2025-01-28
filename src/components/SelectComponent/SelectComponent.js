3b0798995497d8acdb80546ab5acd5ccb42d641e
Vega <obed.vega@teradata.com>
2025-01-28 10:50:14 -0800

    fix issue when you select the option it does not stay selected

--- src/components/SelectComponent/SelectComponent.js
+++ src/components/SelectComponent/SelectComponent.js
@@ -1,57 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import { useLocation } from 'react-router-dom';
 import styles from './styles.module.css';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
-
 export default function SelectComponent() {
   const [selectedOption, setSelectedOption] = useState('');
   const location = useLocation();
-
   const normalizePath = (path) => path.replace(/\/$/, '');
-  const pathsNoAlert = normalizePath(`/quickstarts/`);
   const currentPath = normalizePath(location.pathname);
-  const shouldDisplayAlert = !currentPath.includes(pathsNoAlert);
+  const aiUnlimitedUrl = useBaseUrl('/ai-unlimited/install-ai-unlimited');
+  const fabricUrl = useBaseUrl('/ai-unlimited/fabric/get-started');
 
-  const aiUnlimitedUrl = useBaseUrl('/ai-unlimited/install-ai-unlimited/');
-  const fabricUrl = useBaseUrl('/ai-unlimited/fabric/get-started/');
+  useEffect(() => {
+    if (currentPath === aiUnlimitedUrl) {
+      setSelectedOption('0');
+    } else if (currentPath === fabricUrl) {
+      setSelectedOption('1');
+    }
+  }, [currentPath, aiUnlimitedUrl, fabricUrl]);
 
   const handleSelection = (event) => {
-    const selectedIndex = event.detail.index; 
-    setSelectedOption(selectedIndex);
-
-    if (selectedIndex === 0) {
+    const selectedIndex = event.detail.index?.toString();
+    if (selectedIndex === '0' && currentPath !== aiUnlimitedUrl) {
       window.location.href = aiUnlimitedUrl;
-    } else if (selectedIndex === 1) {
+    } else if (selectedIndex === '1' && currentPath !== fabricUrl) {
       window.location.href = fabricUrl;
     }
   };
 
-  if (!shouldDisplayAlert) return null;
-
   return (
-      <BrowserOnly>
-        {() => {
-          const { Select, CvListItem } = require('@teradata-web/react-components');
+    <BrowserOnly>
+      {() => {
+        const { Select, CvListItem } = require('@teradata-web/react-components');
 
-          return (
-            <Select
-              className={styles.selectInput}
-              outlined
-              label="Platform"
-              value={selectedOption}
-              onSelected={handleSelection}
-            >
-              <CvListItem tabindex="-1" value="0">
-                Cloud service provider
-              </CvListItem>
-              <CvListItem tabindex="-1" value="1">
-                Microsoft Fabric
-              </CvListItem>
-            </Select>
-          );
-        }}
-      </BrowserOnly>
+        return (
+          <Select
+            label="Platform &nbsp;"
+            outlined
+            onSelected={handleSelection}
+            className={styles.selectInput}
+          >
+            <CvListItem value="0" selected={selectedOption === '0'}>
+              Cloud service provider
+            </CvListItem>
+            <CvListItem value="1" selected={selectedOption === '1'}>
+              Microsoft Fabric
+            </CvListItem>
+          </Select>
+        );
+      }}
+    </BrowserOnly>
   );
 }
