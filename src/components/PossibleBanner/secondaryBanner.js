0fac74df162d7548ce4433cbdacf872da65d1e93
Vega, Obed <Obed.Vega+teradata@Teradata.com>
2025-07-10 08:07:05 -0700

    fix hidden paths

--- src/components/PossibleBanner/secondaryBanner.js
+++ src/components/PossibleBanner/secondaryBanner.js
@@ -2,36 +2,34 @@ import styles from './styles.module.css';
 import { useLocation } from 'react-router-dom';
 
 const SecondaryBanner = () => {
-    const location = useLocation();
-    const normalizePath = (path) => path.replace(/\/$/, ''); 
-    const currentPath = normalizePath(location.pathname);
+  const location = useLocation();
+  const currentPath = location.pathname.replace(/\/+$/, ''); 
 
-    const paths = [
-        normalizePath(`/`),
-        normalizePath(`/ai-unlimited`),
-        normalizePath(`/quickstarts`),
-    ];
-    const shouldDisplayAlert = !paths.includes(currentPath);
-    console.log("paths:", paths);
-    console.log("currentPath:", currentPath);
-    console.log("shouldDisplayAlert:", shouldDisplayAlert);
-    console.log(currentPath, paths)
+  const hiddenExactPaths = [
+    '',
+    '/',
+    '/quickstarts',
+    '/ai-unlimited'
+  ];
 
-    if (!shouldDisplayAlert) return null;
+  const isExactPath = (path) => currentPath === path.replace(/\/+$/, '');
+  const shouldDisplayAlert = !hiddenExactPaths.some(isExactPath);
 
-    const quickstartsLink = "https://www.teradata.com/events/possible?utm_source=developer-site&utm_medium=display&utm_campaign=gbl-possible-devrel&utm_content=developer-quickstarts&utm_id=701V500000HTemgIAD";
-    
-        return (
-        <div className={styles.secondaryBanner}>
-                <span className={styles.bannerText}>
-                    Learn how to build the future of AI at Possible 2025. 
-                </span>
-                &nbsp;
-                <a className={styles.bannerTextLink} href={quickstartsLink} target="_blank" rel="noopener noreferrer">
-                    Register now
-                </a>
-        </div>
-    );
+  if (!shouldDisplayAlert) return null;
+
+  const quickstartsLink = "https://www.teradata.com/events/possible?utm_source=developer-site&utm_medium=display&utm_campaign=gbl-possible-devrel&utm_content=developer-quickstarts&utm_id=701V500000HTemgIAD";
+
+  return (
+    <div className={styles.secondaryBanner}>
+      <span className={styles.bannerText}>
+        Learn how to build the future of AI at Possible 2025.
+      </span>
+      &nbsp;
+      <a className={styles.bannerTextLink} href={quickstartsLink} target="_blank" rel="noopener noreferrer">
+        Register now
+      </a>
+    </div>
+  );
 };
 
 export default SecondaryBanner;
