b21d3e0cb13e8fde1b4be3e75b752046ae45f075
oseer williams <oseer.williams@gmail.com>
2025-12-17 14:18:18 -0500

    Fix/remove possible (#205)
    * Remove PossibleBanner and SecondaryBanner components from DevelopersHomepage and Navbar
    
    * Remove PossibleBanner component from QuickstartsPages

--- src/components/DevelopersHomepage/index.js
+++ src/components/DevelopersHomepage/index.js
@@ -2,12 +2,10 @@ import JumpIn from './JumpIn';
 import GetStarted from './GetStarted';
 import TopPicks from './TopPicks';
 import LinksImages from './LinksImg';
-import PossibleBanner from '../PossibleBanner';
 
 export default function DevelopersHomepage() {
   return (
     <>
-    <PossibleBanner />
     <JumpIn />
     <GetStarted />
     <TopPicks />
--- src/components/QuickstartsPages/index.js
+++ src/components/QuickstartsPages/index.js
@@ -4,7 +4,6 @@ import styles from './styles.module.css';
 import Link from '@docusaurus/Link';
 import { Typography } from '@teradata-web/react-components';
 import { translate } from '@docusaurus/Translate';
-import PossibleBanner from '../PossibleBanner';
 
 const FeatureTitle = translate({ message: 'home_page.categories' });
 const Guides = [
@@ -103,7 +102,6 @@ function Feature2({ title, description, href }) {
 export default function Categories() {
   return (
     <>
-      <PossibleBanner />
       <section className={clsx(styles.features, styles.fGuides)}>
         <div className={clsx('container', styles.container)}>
             <br/>
